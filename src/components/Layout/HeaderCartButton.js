0c45000be1f83dbaf3601413a15ec5e3944b3627
syedamariumhasnain <syedamarium27@gmail.com>
2022-01-26 16:48:07 +0500

    added cart-context, cartProvider & implemented on HeaderCartButton

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../context/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  // reduce is a built-in method, allows to transform an array of data
+  // into a single value i.e. initially curNumber is 0 and after each
+  // execution it will be the returned result in previous execution
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span> Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
