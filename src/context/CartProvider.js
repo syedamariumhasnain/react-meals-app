cd8a0a6799005769622411d569492335fe0f722d
syedamariumhasnain <syedamarium27@gmail.com>
2022-01-26 18:14:49 +0500

    Reducer for cart data, Ref + React.forwardRef for amount input

--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -1,13 +1,46 @@
+import { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = () => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCartHandler = () => {};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    // concat() unlike push() do not edit existing array, but 
+    // creates a new array.
+    const updateItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      ...state,
+      items: updateItems,
+      totalAmount: updatedTotalAmount,
+    }
+  } else if (action.type === "REMOVE") {
+
+  }
+  return state;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartActions] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartActions({ type: "ADD", item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartActions({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
